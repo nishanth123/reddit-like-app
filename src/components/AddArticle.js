de4dccd92b773fb00ae941f5c95c3d23dd275ee9
nishanth <nishanth@codeastra.com>
2018-02-18 11:50:56 +0530

    add-article control created

--- src/components/AddArticle.js
+++ src/components/AddArticle.js
@@ -7,53 +7,6 @@ import { createStore } from 'redux';
 import { connect } from 'react-redux';
 import  articleApp  from './../reducers';
 
-class Child extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.onClickSubmitButton = this.onClickSubmitButton.bind(this);
-
-    this.state = { 
-      showing: false 
-    };
-  }
-
-  onClickSubmitButton(){
-    console.log('test-2');
-
-    this.setState(
-      { 
-        showing: true 
-      }
-    );
-  }
-
-  render() {
-    const { showing } = this.state;
-    return (
-      <div id="div_2">
-      <br/>
-      <input type="text" placeholder="Add the article title here" className="block_text"></input>
-      <br/><br/>
-      <input type="text" placeholder="Add the article text here" className="block_text"></input>
-      <br/><br/>
-      <button 
-        type="button" 
-        onClick={() => 
-        this.onClickSubmitButton()}
-      >
-        Submit
-      </button>
-      { showing 
-                    ? <div>This is visible</div>
-                    : null
-      }
-
-    </div>
-    );
-  }
-}
-
 export default class AddArticle extends React.Component {
   constructor(props) {
     super(props);
@@ -61,51 +14,65 @@ export default class AddArticle extends React.Component {
     this.onClickAddButton = this.onClickAddButton.bind(this);
 
     this.state = {
-      error: undefined,
       tempArticle: undefined,
-      childVisible: false,
-      parentVisible: true
+      toggle: true
     };
   }
 
   onClickAddButton(){
-    console.log('test-1');
-
     this.setState(
       prevState => (
         { 
-          childVisible: !prevState.childVisible,
-          parentVisible: !prevState.parentVisible 
+          toggle: false
         }
       )
     );
+
+    console.log(this.state);
     
+  }
+
+  onClickSubmitButton(){
+    this.setState(
+      prevState => (
+        { 
+          toggle: true 
+        }
+      )
+    );
   }
 
   // this is the render method
   render() {
-    
     return (
-        
-          <div id="div_1">
+          <div>
+            <div id="div_1" className={this.state.toggle ? 'visible' : 'hidden'}>
+              <br/><br/>
+              <button 
+                type="button" 
+                onClick={() => 
+                this.onClickAddButton()}
+              >
+                Add Article
+              </button>
+            
+            </div>
+            <div id="div_2" className={this.state.toggle ? 'hidden' : 'visible'}>
+            <br/>
+            <input type="text" placeholder="Add the article title here" className="block_text"></input>
+            <br/><br/>
+            <input type="text" placeholder="Add the article text here" className="block_text"></input>
             <br/><br/>
             <button 
               type="button" 
               onClick={() => 
-              this.onClickAddButton()}
+              this.onClickSubmitButton()}
             >
-              Add Article
+              Submit
             </button>
-            {
-            
-            this.state.childVisible
-            ? <Child />
-            : null
-
-            }
-          </div>
-
+      
+            </div>
+         </div>
     );
-
   }
 }
